043f8cae8401634c9719173480bbc608fc5d73ea
Nur Dilah Zaini <90826642+justdilah@users.noreply.github.com>
2024-01-11 02:56:36 +0800

    Revert "Delete ratemoviesapp directory"
    This reverts commit ed30a91b1835b07dcbbc5039e4928f94341137cf.

---
+++ ratemoviesapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from "./components/Table";
+function App() {
+  return (
+      <body>
+        <h1 class="display-4">My Movie Reviews</h1>
+        <h3>(for current movies)</h3>
+          <br/>
+        <Table/>
+      </body>
+  );
+}
+
+export default App;
